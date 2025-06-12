fcbcbe35c471f1c64221988936b43c252a9054e2
lisefourniercarnoy <lisefourniercarnoy@gmail.com>
2025-06-12 16:29:19 +0800

    script mod

---
+++ r/Length_modelling/length_model_00_checkEM.R
@@ -0,0 +1,257 @@
+# -----------------------------------------------------------------------------
+
+# Project: BRUV sampling design comparison
+# Data:    BRUV habitat & abundance data.
+# Task:    Check the raw EventMeasure files
+# Author:  Lise Fournier-Carnoy / from https://globalarchivemanual.github.io/CheckEM/articles/r-workflows/check-fish-point.html
+# Date:    June 2025
+
+# -----------------------------------------------------------------------------
+
+# Status: 
+
+# -----------------------------------------------------------------------------
+
+# install.packages('remotes')
+library('remotes')
+options(timeout = 9999999)
+# remotes::install_github("GlobalArchiveManual/CheckEM")
+library(CheckEM)
+library(tidyverse)
+library(googlesheets4)
+library(sf)
+library(terra)
+library(here)
+
+# Clear memory
+rm(list=ls())
+
+name <- "2024_waatu_BRUVs"
+tidy_filename <- "2024_waatu_commonwealth/" # where to save the tidy data THIS SHOULD BE THE SAME IN raw/ AND tidy/
+
+## Format metadata ------------------------------------------------------------
+
+metadata <- read_metadata(here::here(paste0("data/raw/", tidy_filename)), method = "BRUVs") %>% # Change here to "DOVs"
+  dplyr::select(campaignid, sample, status, longitude_dd, latitude_dd, date_time, location, site, depth_m, successful_count, successful_length, successful_habitat_forwards, successful_habitat_backwards) %>%
+  glimpse()
+saveRDS(metadata, file = here::here(paste0("data/tidy/", tidy_filename, name, "_metadata.rds")))
+
+
+## Format points --------------------------------------------------------------
+
+points <- read_points(here::here(paste0("data/raw/", tidy_filename))) %>%
+  glimpse()
+
+if(nrow(points) > 1){
+  maxn_points <- points %>% 
+    dplyr::group_by(campaignid, sample, filename, periodtime, frame, family, genus, species) %>% # If you have MaxN'd by stage (e.g. Adult, Juvenile) add stage here
+    dplyr::mutate(number = as.numeric(number)) %>%
+    dplyr::summarise(maxn = sum(number)) %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(campaignid, sample, family, genus, species) %>%
+    dplyr::slice(which.max(maxn)) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!is.na(maxn)) %>%
+    dplyr::select(-frame) %>%
+    tidyr::replace_na(list(maxn = 0)) %>%
+    dplyr::mutate(maxn = as.numeric(maxn)) %>%
+    dplyr::filter(maxn > 0) %>%
+    dplyr::inner_join(metadata, by = join_by(campaignid, sample)) %>%
+    dplyr::filter(successful_count %in% c("Yes")) %>% 
+    dplyr::filter(maxn > 0) %>%
+    dplyr::select(campaignid, sample, family, genus, species, maxn) %>%
+    dplyr::glimpse()
+}
+maxn <- bind_rows(get0("maxn_points"), get0("maxn_counts")) # this works even if you only have one type of data
+
+count <- maxn %>%
+  dplyr::mutate(family = ifelse(family %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "Unknown", as.character(family))) %>%
+  dplyr::mutate(genus = ifelse(genus %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "Unknown", as.character(genus))) %>%
+  dplyr::mutate(species = ifelse(species %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "spp", as.character(species))) %>%
+  dplyr::select(campaignid, sample, family, genus, species, maxn) %>%
+  tidyr::complete(nesting(campaignid, sample), nesting(family, genus, species)) %>%
+  tidyr::replace_na(list(maxn = 0)) %>%
+  group_by(campaignid, sample, family, genus, species) %>%
+  dplyr::summarise(count = sum(maxn)) %>%
+  ungroup() %>%
+  mutate(scientific = paste(family, genus, species, sep = " "))%>%
+  dplyr::select(campaignid, sample, scientific, count)%>%
+  spread(scientific, count, fill = 0)
+
+count_families <- maxn %>%
+  dplyr::mutate(scientific = paste(family, genus, species, sep = " ")) %>%
+  filter(!(family %in% "Unknown")) %>%
+  dplyr::select(c(family, genus, species, scientific)) %>%
+  distinct()
+
+complete_count <- count %>%
+  pivot_longer(names_to = "scientific", values_to = "count",
+               cols = 3:ncol(.)) %>%
+  inner_join(count_families, by = c("scientific")) %>%
+  full_join(metadata)%>%
+  glimpse()
+
+
+## Format lengths -------------------------------------------------------------
+
+
+# currently waatu lengths not done, run when they're checked
+em_length3dpoints <- read_em_length(here::here(paste0("data/raw/", tidy_filename))) %>%                   
+  dplyr::select(-c(comment))%>% # there is a comment column in metadata, so you will need to remove this column from EM data
+  dplyr::inner_join(metadata, by = join_by(sample, campaignid)) %>%
+  dplyr::filter(successful_length %in% "Yes") %>%
+  # dplyr::rename(length_mm = length) %>%
+  glimpse() 
+
+length <- bind_rows(get0("em_length3dpoints"), get0("gen_length")) # this works even if you only have one type of data
+
+complete_length <- length %>%
+  dplyr::mutate(family = ifelse(family %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "Unknown", as.character(family))) %>%
+  dplyr::mutate(genus = ifelse(genus %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "Unknown", as.character(genus))) %>%
+  dplyr::mutate(species = ifelse(species %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "spp", as.character(species))) %>%
+  dplyr::filter(!family %in% "Unknown")%>%
+  # First make one row for every length measurement
+  dplyr::mutate(number = as.numeric(number)) %>%
+  tidyr::uncount(number) %>%
+  dplyr::mutate(number = 1) %>% 
+  # Add in missing samples
+  dplyr::right_join(metadata) %>%
+  dplyr::filter(successful_length %in% "Yes") %>%
+  # Complete the data (add in zeros for every species)
+  dplyr::select(campaignid, sample, family, genus, species, length_mm, number, any_of(c("range", "rms", "precision"))) %>% # this will keep EM only columns
+  tidyr::complete(nesting(campaignid, sample), nesting(family, genus, species)) %>%
+  replace_na(list(number = 0)) %>%
+  ungroup() %>%
+  dplyr::filter(!is.na(number)) %>%
+  dplyr::mutate(length_mm = as.numeric(length_mm)) %>%
+  left_join(., metadata) %>%
+  glimpse()
+
+
+## Quality control checks -----------------------------------------------------
+
+number_of_samples <- metadata %>%
+  dplyr::distinct(campaignid, sample)
+
+message(paste(nrow(number_of_samples), "unique samples in the metadata"))
+
+duplicate_samples <- metadata %>%
+  dplyr::group_by(campaignid, sample) %>%
+  dplyr::summarise(n = n()) %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(n > 1)
+message(paste(nrow(duplicate_samples), "samples duplicated in the metadata"))
+
+
+metadata_samples <- metadata %>%
+  dplyr::select(campaignid, sample, dplyr::any_of(c("opcode", "period")), 
+                successful_count, successful_length) %>%
+  distinct()
+samples <- maxn %>%
+  distinct(campaignid, sample)
+missing_count <- anti_join(metadata_samples, samples, by = join_by(campaignid, sample))
+message(paste(nrow(missing_count), "samples in the metadata missing count data"))
+
+
+metadata_samples <- metadata %>%
+  dplyr::select(campaignid, sample, dplyr::any_of(c("opcode", "period")), 
+                successful_count, successful_length) %>%
+  distinct()
+samples <- length %>%
+  distinct(campaignid, sample)
+missing_length <- anti_join(metadata_samples, samples, by = join_by(campaignid, sample))
+message(paste(nrow(missing_length), "samples in metadata missing length data"))
+
+
+missing_metadata <- anti_join(samples, metadata_samples, by = join_by(campaignid, sample))
+message(paste(nrow(missing_metadata), "samples in length data missing metadata"))
+
+periods <- read_periods(here::here("r-workflows/data/raw/")) %>%
+  glimpse()
+periods_without_end <- periods %>%
+  dplyr::filter(has_end == 0)
+message(paste(nrow(periods_without_end), "periods without an end"))
+
+metadata_samples <- metadata %>%
+  dplyr::select(campaignid, sample, dplyr::any_of(c("opcode", "period")), successful_count, successful_length) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(sample = as.factor(sample))
+periods_samples <- periods %>%
+  dplyr::select(campaignid, sample, dplyr::any_of(c("opcode", "period"))) %>%
+  distinct()
+missing_periods <- anti_join(metadata_samples, periods_samples) %>%
+  dplyr::select(!sample)
+message(paste(nrow(missing_periods), "samples missing period"))
+
+
+points_outside_periods <- points %>%
+  dplyr::filter(period %in% c("NA", NA, NULL, "")) %>%
+  dplyr::select(campaignid, dplyr::any_of(c("opcode", "period")), family, genus, species, number, frame)
+message(paste(nrow(points_outside_periods), "points outside a period"))
+glimpse(points_outside_periods)
+
+
+lengths_outside_periods <- em_length3dpoints %>%
+  dplyr::filter(period %in% c("NA", NA, NULL, "")) %>%
+  dplyr::select(campaignid, dplyr::any_of(c("opcode", "period")), family, genus, species, number)
+message(paste(nrow(lengths_outside_periods), "lengths/3D points outside period"))
+glimpse(lengths_outside_periods)
+
+
+period_length <- 60 # in minutes
+periods_wrong <- periods %>%
+  dplyr::select(campaignid, dplyr::any_of(c("opcode", "period")), time_start, time_end, has_end) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(period_time = round(time_end - time_start)) %>%
+  dplyr::filter(!period_time %in% period_length)
+message(paste(nrow(periods_wrong), "periods not", period_length, "minutes long"))
+
+
+total_count <- sum(complete_count$count)
+message(paste(total_count, "fish counted in the count data"))
+
+total_length <- sum(complete_length$number)
+message(paste(total_length, "fish counted in the length data"))
+
+
+points_without_number <- points %>%
+  filter(number %in% c("NA", NA, 0, NULL, "", " "))
+message(paste(nrow(points_without_number), "points in the _Points.txt file that do not have a number"))
+glimpse(points_without_number)
+
+lengths_without_number <- em_length3dpoints %>%
+  filter(number %in% c("NA", NA, 0, NULL, "", " "))
+message(paste(nrow(lengths_without_number), "lengths or 3D points in the EMObs that do not have a number"))
+
+synonyms_in_count <- dplyr::left_join(complete_count, CheckEM::aus_synonyms) %>%
+  dplyr::filter(!is.na(genus_correct)) %>%
+  dplyr::mutate('old name' = paste(family, genus, species, sep = " ")) %>%
+  dplyr::mutate('new name' = paste(family_correct, genus_correct, species_correct, sep = " ")) %>%
+  dplyr::select('old name', 'new name') %>%
+  dplyr::distinct()
+message(paste(nrow(synonyms_in_count), "synonyms used in the count data"))
+
+
+synonyms_in_length <- dplyr::left_join(complete_length, CheckEM::aus_synonyms) %>%
+  dplyr::filter(!is.na(genus_correct)) %>%
+  dplyr::mutate('old name' = paste(family, genus, species, sep = " ")) %>%
+  dplyr::mutate('new name' = paste(family_correct, genus_correct, species_correct, sep = " ")) %>%
+  dplyr::select('old name', 'new name') %>%
+  dplyr::distinct()
+
+incorrect_lengths <- left_join(complete_length, create_min_max(CheckEM::australia_life_history, minimum = 0.15, maximum = 0.85)) %>%
+  dplyr::filter(length_mm < min_length_mm | length_mm > max_length_mm) %>%
+  mutate(reason = ifelse(length_mm < min_length_mm, "too small", "too big")) %>%
+  dplyr::select(campaignid, sample, family, genus, species, length_mm, min_length_mm, max_length_mm, length_max_mm, reason, any_of(c("em_comment", "frame_left")), length_max_mm) %>%
+  mutate(difference = ifelse(reason %in% c("too small"), (min_length_mm - length_mm), (length_mm - max_length_mm))) %>%
+  dplyr::mutate(percent_of_fb_max = (length_mm/length_max_mm)*100) %>%
+  dplyr::left_join(metadata) %>%
+  dplyr::select(campaignid, dplyr::any_of(c("opcode", "period")), family, genus, species, length_mm, min_length_mm, max_length_mm, length_max_mm, reason, any_of(c("em_comment", "frame_left")), difference, percent_of_fb_max)
+
+## Save the checked, clean data -----------------------------------------------
+saveRDS(complete_count,
+        file = here::here(paste0("r-workflows/data/staging/",
+                                 name, "_complete-count.rds")))
+saveRDS(complete_length,
+        file = here::here(paste0("r-workflows/data/staging/",
+                                 name, "_complete-length.rds")))
--- r/Length_modelling/length_model_XX_plotting.R
+++ r/Length_modelling/length_model_XX_plotting.R
@@ -18,6 +18,9 @@ library(ggplot2)
 library(tmap)
 library(terra)
 
+colour_palette <- c("#6B0504", "#08415C", "#3E6990", "#fff2cc", "#f1c232", "#7f6000")
+saveRDS(colour_palette, "chapter_colours.rds")
+
 ## Basic map of study area ----------------------------------------------------
 
 aus                   <- st_read("data/spatial/shapefiles/wadandi_land.shp")
@@ -112,4 +115,194 @@ ggplot() +
         legend.box.spacing = unit(0, "cm"),
         legend.key.size = unit(0.5, "cm"),
         legend.text = element_text(size = 10))
+
+
+# real bruv locations (correct colours)
+
+
+aus             <- st_read("data/spatial/shapefiles/wadandi_land.shp") # land shapefile
+com_samp_area   <- st_read("QGIS layers/polys/com_comp_analysis_limits.shp")
+AMP             <- st_read("QGIS layers/polys/AMP_local.shp") # real SZs
+MPA             <- st_read("QGIS layers/polys/swc_sanctuaryzones.shp"); MPA <- st_zm(MPA); st_crs(MPA) <- 4326 # real SZs
+
+pref            <- st_read("QGIS layers/2024_pref_BRUVs.shp") # real BRUV state points
+spabal          <- st_read("QGIS layers/2024_spabal_BRUVs.shp") # real BRUV commonwealth points
+MaxN            <- read.csv("data/tidy/2024_geographe_all_tidy_maxn.csv") # all MaxNs
+
+
+library(ggpubr) # for ggarrange
+library(grid)
+library(imager)
+library(ggspatial) # for scale bars
+
+palette <- c("#E5B25D", "#A3320B", "#6B0504", "#394F49", "#65743A")
+
+# Load images as raster grobs
+image1 <- rasterGrob(load.image("photos/wadandi_ranger_BRUV.JPG"), interpolate = TRUE)
+image2 <- rasterGrob(load.image("photos/BRUV_photo_yijarup_skippy.PNG"), interpolate = TRUE)
+
+# Arrange images into ggplot objects
+image_plot1 <- ggplot() + annotation_custom(image1) + theme_void()
+image_plot2 <- ggplot() + annotation_custom(image2) + theme_void() 
+
+plot <- ggplot() +
+  geom_sf(data = com_samp_area,
+          aes(fill = 'Community \ncomposition \nanalysis limits'), color = "#A3320B", lwd = 1) +
+  geom_sf(data = AMP[AMP$ZoneName == 'National Park Zone',],
+          aes(fill = 'No-take \nzones'), alpha = 0.1, color = "#65743A", lwd = 1) +
+  geom_sf(data = MPA,
+          aes(fill = 'No-take \nzones'), alpha = 0.1, color = "#65743A", lwd = 1) +
+  geom_sf(data = pref[pref$longitude > 115.3,],
+          aes(color = 'Preferential \nsampling'), size = 1, shape = 16) +
+  geom_sf(data = spabal[spabal$SD == 'spabal',],
+          aes(color = 'Spatially \nbalanced \nsampling'), size = 1, shape = 16) +
+  geom_sf(data = spabal[spabal$SD == 'pref',],
+          aes(color = 'Preferential \nsampling'), size = 1, shape = 16) +
+  geom_sf(data = aus, fill = "lightgrey", color = "darkgray", lwd = 1) +  # Move 'aus' layer to the top
+  coord_sf(crs = 4326, xlim = ext(spabal)[1:2], ylim = ext(spabal)[3:4]) +
+  theme_minimal() +
+  scale_fill_manual(values = c('No-take \nzones' = "#65743A", 'Community \ncomposition \nanalysis limits' = 'transparent')) +
+  scale_color_manual(values = c('Preferential \nsampling' = "#E5B25D", 'Spatially \nbalanced \nsampling' = "#6B0504")) +
+  labs(fill = '', color = '') +
+  theme(legend.position = "bottom",
+        legend.box = "horizontal",
+        legend.box.spacing = unit(0, "cm"),
+        legend.key.size = unit(0.5, "cm"),
+        legend.text = element_text(size = 10)) +
+  annotation_scale(location = "br", width_hint = 0.2)  # Adds a scale bar
+
+
+
+plot_zoom <- ggplot() + # zoomed in
+  geom_sf(data = com_samp_area,
+          aes(fill = 'Community \ncomposition \nanalysis limits'), color = "#A3320B", lwd = 1) +
+  geom_sf(data = AMP[AMP$ZoneName == 'National Park Zone',],
+          aes(fill = 'No-take \nzones'), alpha = 0.1, color = "#65743A", lwd = 1) +
+  geom_sf(data = MPA,
+          aes(fill = 'No-take \nzones'), alpha = 0.1, color = "#65743A", lwd = 1) +
+  geom_sf(data = pref[pref$longitude > 115.3,],
+          aes(color = 'Preferential \nsampling'), size = 1, shape = 16) +
+  geom_sf(data = spabal[spabal$SD == 'spabal',],
+          aes(color = 'Spatially \nbalanced \nsampling'), size = 1, shape = 16) +
+  geom_sf(data = spabal[spabal$SD == 'pref',],
+          aes(color = 'Preferential \nsampling'), size = 1, shape = 16) +
+  geom_sf(data = aus, fill = "lightgrey", color = "darkgray", lwd = 1) +
+  coord_sf(crs = 4326, xlim = ext(com_samp_area)[1:2], ylim = ext(com_samp_area)[3:4]) +
+  theme_minimal() +
+  scale_fill_manual(values = c('No-take \nzones' = "#65743A", 'Community \ncomposition \nanalysis limits' = 'transparent')) +
+  scale_color_manual(values = c('Preferential \nsampling' = "#E5B25D", 'Spatially \nbalanced \nsampling' = "#6B0504")) +
+  labs(fill = '', color = '') +
+  theme(legend.position = "bottom",
+        legend.box = "horizontal",
+        legend.box.spacing = unit(0, "cm"),
+        legend.key.size = unit(0.5, "cm"),
+        legend.text = element_text(size = 10),
+        axis.text.x = element_blank(),  # Remove x-axis 
+        axis.text.y = element_blank()   # Remove y-axis 
+  ) +
+  annotation_scale(location = "br", width_hint = 0.2)  # Adds a scale bar
+
+# Combine the two plots with a shared legend
+combined_plots <- ggarrange(
+  plot, plot_zoom,
+  ncol = 2,
+  common.legend = TRUE,
+  legend = "bottom",
+  labels = c("C", "D"),
+  widths = c(1.45, 1)  # Equal heights for both plots
+)
+
+# Arrange everything
+final_layout <- ggarrange(
+  ggarrange(image_plot1, 
+            image_plot2, 
+            ncol = 2, 
+            labels = c("A", "B"),
+            widths = c(1, 1)),
+  combined_plots,
+  nrow = 2, 
+  heights = c(1.1, 1.6)
+)
+
+# Display the final layout
+final_layout
+
+
+
+### size distributions ----
+
+## Files used in this script --------------------------------------------------
+
+file_fish_length        <- "data/raw/2024_state/2024_02_NgariCapes.MP.Monitoring_stereoBRUVs_Lengths.txt" # obtained from 00_checkem
+file_metadata           <- "data/raw/2024_state/2024_02_NgariCapes.MP.Monitoring_stereoBRUVs_Metadata.csv" # obtained from MEGlab labsheets
+
+file_obs_hab            <- "data/raw/2024_commonwealth/2024-04_Geographe_stereo-BRUVs_forwards_Dot Point Measurements.txt" # output form TransectMeasure
+
+file_250m_bathy         <- "data/spatial/rasters/GB-SW_250mBathy.tif" # from Geoscience Australia
+file_250m_bathy_deriv   <- "data/spatial/rasters/2014_geographe_bathymetry-derivatives.RDS" # From Claude's code
+file_lidar_bathy        <- "data/spatial/rasters/LiDAR_geo_compressed.tif" # Too big for git
+
+
+## Presence/Absence data ------------------------------------------------------
+
+L50 <- c('Chrysophrys auratus' = 566, # L50 from Wakefield et al. 2015 - males 585, females 566
+         'Glaucosoma hebraicum' = 0, #301, # L50 from Hesp 2002 - males 320, females 301 - but using all fish because papers find NTZ makes dhuies more abundant not bigger
+         "Ophthalmolepis lineolatus" = 184 # l50 from females, Morton 2008
+) 
+
+metadata <- read.csv(file_metadata) %>% 
+  dplyr::filter(successful_count == "Yes" & successful_length == "Yes",
+                grepl("EGB", opcode)) %>% 
+  dplyr::select("opcode", "latitude_dd", "longitude_dd", "status", "depth_m") %>% 
+  glimpse()
+
+length_data <- read.table(file_fish_length, sep = "\t", header = T) %>%
+  rename(opcode = OpCode) %>% 
+  mutate(full_spp = paste(Genus, Species)) %>% 
+  dplyr::filter(grepl("EGB", opcode)) %>% 
+  glimpse()
+
+names(length_data)
+unique(length_data$full_spp)
+dat <- length_data %>% 
+  # Select species present in L50
+  filter(full_spp %in% names(L50)) %>% 
+  
+  # Join the metadata with the length data
+  left_join(metadata %>% distinct(opcode, longitude_dd, latitude_dd), by = "opcode") #%>% 
+  
+  # Separate into size categories, treating missing lengths as immature
+  # group_by(longitude_dd, latitude_dd, full_spp, opcode) %>% 
+  # summarise(
+  #   count_mature = sum(coalesce(Length, 0) > L50[full_spp], na.rm = TRUE),
+  #   count_immature = sum(coalesce(Length, 0) <= L50[full_spp], na.rm = TRUE),
+  #   .groups = "drop"
+  # ) %>% 
+  # 
+  # # Ensure that all full_spp are present even if no data is available for the species
+  # complete(opcode, full_spp, fill = list(count_mature = 0, count_immature = 0)) %>% 
+  # complete(opcode = unique(metadata$opcode), full_spp = full_spp, fill = list(count_mature = 0, count_immature = 0)) %>% 
+  
+  # Fill in missing longitude and latitude
+  # left_join(metadata %>% distinct(opcode, longitude_dd, latitude_dd), by = "opcode") %>% 
+  # mutate(
+  #   longitude = coalesce(longitude_dd.x, longitude_dd.y),
+  #   latitude = coalesce(latitude_dd.x, latitude_dd.y)
+  # ) %>%
+  # dplyr::select(-longitude_dd.x, -latitude_dd.x, -longitude_dd.y, -latitude_dd.y) %>% 
+  # 
+  # # Create an ID column
+  # mutate(ID = row_number()) %>% 
+  # glimpse()
+
+length(unique(dat$opcode)) == length(unique(metadata$opcode)) # should be true. if not there are opcodes missing.
+
+ggplot(dat, aes(x = longitude_dd, y = latitude_dd, size = Length)) +
+  facet_wrap(~full_spp) +
+  geom_point()
+
+saveRDS(dat, "data/tidy/mature_presence_latlong.rds") # Needed for 03_gam
+
+summary(as.factor(dat$full_spp[dat$count_mature>0]))
+
 
